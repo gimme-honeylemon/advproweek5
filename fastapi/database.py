f22a9a48872f25428ea5a68a345dfe5103f5a237
gimme-honeylemon <67011734@kmitl.ac.th>
2025-08-04 16:40:34 +0700

    file uploading

--- fastapi/database.py
+++ fastapi/database.py
@@ -29,9 +29,11 @@ async def insert_user(username: str, password_hash: str, email: str):
     return await database.fetch_one(query=query, values=values)
 
 # Function to select a user by user_id from the users table
-async def get_user(user_id: int):
-    query = "SELECT * FROM users WHERE user_id = :user_id"
-    return await database.fetch_one(query=query, values={"user_id": user_id})
+async def get_user(username: str):
+   query = "SELECT * FROM users WHERE username = :username"
+   return await database.fetch_one(query=query, values={"username": username})
+
+
 
 # Function to update a user in the users table
 async def update_user(user_id: int, username: str, password_hash: str, email: str):
